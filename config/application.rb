6f28b8d7b9afe9c57f1b15d35058d279e395e44c
github username <github email address>
2018-09-27 10:08:28 -0500

    Done.

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,15 @@ Bundler.require(*Rails.groups)
 
 module RailsLayoutsAndTemplatesLab
   class Application < Rails::Application
+    config.generators do |g|
+      g.test_framework false
+      g.helper false
+      g.stylesheet false
+      g.assets false
+      g.migration false
+
+
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
